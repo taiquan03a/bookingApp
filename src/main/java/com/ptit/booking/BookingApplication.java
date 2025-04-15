dd612c94984c9703fa98369b91836c33da358c19
DuongNo0b23 <lamtiendung11082002@gmail.com>
2025-04-15 14:58:28 +0700

    duong commit

--- src/main/java/com/ptit/booking/BookingApplication.java
+++ src/main/java/com/ptit/booking/BookingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookingApplication {
+public class BookingApplication {	
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingApplication.class, args);
